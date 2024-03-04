2b753a069d8a1fdf439931ce94db200df7a4a3a8
sprogrammer12 <9727991+sprogrammer12@users.noreply.github.com>
2024-03-04 12:26:03 +1100

    daily codewar challenge

---
+++ 8kyu/covert-boolean-values-to-strings.js
@@ -0,0 +1,10 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// My Solutions
+
+function boolToWord( bool ){
+  
+  return bool ? "Yes" : "No"
+}
+
+const boolToWord = bool => bool ? "Yes" : "No"
